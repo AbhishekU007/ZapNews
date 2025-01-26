fda2ec16e79527cfb5d85769418cb873ff72f1a0
Abhishek Upadhyay <abhishekupadhyay0007@gmail.com>
2025-01-26 13:26:17 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -4,13 +4,19 @@ import './App.css';
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
 import News from './components/News';
+import{
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <Navbar/>
-        <News/>
+        <News pageSize={9} country="us"/>
       </div>
     )
   }
